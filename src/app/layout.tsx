fcb5c40a8631cda12da2ff0ad2692ee9dfaae4e2
Sebastian Zhunaula <zhunaulasebastian31@gmail.com>
2026-01-24 03:45:40 +0000

    ahora para el texto usa esta fuente de Aldrich <link rel="preconnect" hr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Playfair+Display:wght@400;700;900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Aldrich&family=Oswald:wght@200..700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" rel="stylesheet" />
       </head>
       <body className={cn('font-body antialiased overflow-x-hidden', 'dark:bg-background-dark')}>
